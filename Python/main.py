439564fb52004ed3e61d84088559d86e5ad68b67
Rémi MEVAERE <remi@mevaere.fr>
2024-01-01 18:12:03 +0100

    Correct bugs with time calculation

--- Python/main.py
+++ Python/main.py
@@ -17,11 +17,14 @@ from fiboCythonFull import fibonacci_interop_cython_full
 from array import array
 import ctypes
 import os
+
 current_directory = os.getcwd()
 
 from pythonnet import load
+
 load("coreclr")
 import clr  # Import CLR from Python.NET
+
 clr.AddReference('System')
 clr.AddReference(os.path.join(current_directory, 'DllFibonacci.dll'))
 from DllFibonacci import MyFiboClass
@@ -30,7 +33,8 @@ from System import UInt64, Boolean, Single, Double
 from System.Runtime.InteropServices import GCHandle, GCHandleType
 
 # Load the DLL
-lib = ctypes.CDLL(os.path.join(current_directory, 'InteropFibonacciWinCPP.dll'))  # Update with the correct path to your DLL
+lib = ctypes.CDLL(
+    os.path.join(current_directory, 'InteropFibonacciWinCPP.dll'))  # Update with the correct path to your DLL
 # Set the argument types for the fibonacci_interop function
 lib.fibonacci_interop_cpp.argtypes = [
     ctypes.c_ulonglong, ctypes.c_ubyte, ctypes.c_longlong, ctypes.c_ulonglong, ctypes.c_ubyte,
@@ -49,10 +53,8 @@ lib2.fibonacci_interop_asm.argtypes = [
 ]
 lib2.fibonacci_interop_asm.restype = ctypes.c_int
 
-# List of time taken for the test to calculate the mean and the standard deviation
-listTimeCount: List[float] = []
 
-def execute_loop(nameTest: str, functionToTest)->None:
+def execute_loop(nameTest: str, functionToTest) -> None:
     """Execute a test loop for a given function and print the results.
 
    This function performs a test loop for a specified function and prints the results.
@@ -70,6 +72,9 @@ def execute_loop(nameTest: str, functionToTest)->None:
    :return: None
       This function does not return anything but prints the test results."""
 
+    # List of time taken for the test to calculate the mean and the standard deviation
+    listTimeCount: List[float] = []
+
     for _ in range(parameters.numberRun):
         start_time: float = time.time()  # Start the timer
         fbRet, arTerms, arPrimes, arError, goldenNbr = functionToTest()
@@ -78,11 +83,13 @@ def execute_loop(nameTest: str, functionToTest)->None:
 
     printResults(arPrimes, arTerms, goldenNbr, parameters.fiboMaxTerms, listTimeCount, nameTest)
 
+
 def execute_python():
     """Execute the test in Python."""
     return fibonacci_interop_python(parameters.fiboStart, parameters.fiboMaxTerms,
-                                                                  parameters.fiboMaxValue,
-                                                                  parameters.fiboMaxFactor, parameters.fiboNbrOfLoops)
+                                    parameters.fiboMaxValue,
+                                    parameters.fiboMaxFactor, parameters.fiboNbrOfLoops)
+
 
 def execute_cython():
     """Execute the test in Cython."""
@@ -90,7 +97,9 @@ def execute_cython():
     arPrimes = array('b', [0] * parameters.fiboMaxTerms * 50)  # 'b' for signed char
     arError = array('d', [0] * parameters.fiboMaxTerms)  # 'f' for double
 
-    fbRet, goldenNbr = fibonacci_interop_cython(parameters.fiboStart, parameters.fiboMaxTerms, parameters.fiboMaxValue, parameters.fiboMaxFactor, parameters.fiboNbrOfLoops, arTerms, arPrimes, arError)
+    fbRet, goldenNbr = fibonacci_interop_cython(parameters.fiboStart, parameters.fiboMaxTerms, parameters.fiboMaxValue,
+                                                parameters.fiboMaxFactor, parameters.fiboNbrOfLoops, arTerms, arPrimes,
+                                                arError)
     return fbRet, arTerms, arPrimes, arError, goldenNbr
 
 
@@ -103,11 +112,14 @@ def execute_dotnet():
     # Initialize the out parameter as a reference
     goldenNbr = Array[Double]([0.0])
 
-    fibonacciResult = MyFiboClass.fibonacci_interop_cs(parameters.fiboStart, parameters.fiboMaxTerms, parameters.fiboMaxValue, parameters.fiboMaxFactor, parameters.fiboNbrOfLoops, arTerms,
-                                                   arPrimes, arError)
+    fibonacciResult = MyFiboClass.fibonacci_interop_cs(parameters.fiboStart, parameters.fiboMaxTerms,
+                                                       parameters.fiboMaxValue, parameters.fiboMaxFactor,
+                                                       parameters.fiboNbrOfLoops, arTerms,
+                                                       arPrimes, arError)
 
     result = fibonacciResult.Result
     return result, arTerms, arPrimes, arError, fibonacciResult.GoldenNumber
+
 
 def execute_cpp():
     arTerms = (ctypes.c_ulonglong * (parameters.fiboMaxTerms * 50))()  # Adjust size as needed
@@ -115,8 +127,9 @@ def execute_cpp():
     arError = (ctypes.c_double * parameters.fiboMaxTerms)()  # Adjust size as needed
     goldenNbr = ctypes.c_double()
 
-    result = lib.fibonacci_interop_cpp(parameters.fiboStart, parameters.fiboMaxTerms, parameters.fiboMaxValue, parameters.fiboMaxFactor, parameters.fiboNbrOfLoops, arTerms, arPrimes, arError,
-                                   ctypes.byref(goldenNbr))
+    result = lib.fibonacci_interop_cpp(parameters.fiboStart, parameters.fiboMaxTerms, parameters.fiboMaxValue,
+                                       parameters.fiboMaxFactor, parameters.fiboNbrOfLoops, arTerms, arPrimes, arError,
+                                       ctypes.byref(goldenNbr))
     return result, arTerms, arPrimes, arError, goldenNbr.value
 
 
@@ -126,30 +139,38 @@ def execute_asm():
     arError = (ctypes.c_double * parameters.fiboMaxTerms)()  # Adjust size as needed
     goldenNbr = ctypes.c_double()
 
-    result = lib2.fibonacci_interop_asm(parameters.fiboStart, parameters.fiboMaxTerms, parameters.fiboMaxValue, parameters.fiboMaxFactor, parameters.fiboNbrOfLoops, arTerms, arPrimes, arError,
-                                       ctypes.byref(goldenNbr))
+    result = lib2.fibonacci_interop_asm(parameters.fiboStart, parameters.fiboMaxTerms, parameters.fiboMaxValue,
+                                        parameters.fiboMaxFactor, parameters.fiboNbrOfLoops, arTerms, arPrimes, arError,
+                                        ctypes.byref(goldenNbr))
 
     return result, arTerms, arPrimes, arError, goldenNbr.value
+
 
 def main_dotnet():
     nameTest: str = "Dotnet Core"
     execute_loop(nameTest, execute_dotnet)
 
+
 def main_cython_full():
     """Execute the test in Cython Full Version."""
-    fibonacci_interop_cython_full(parameters.fiboStart, parameters.fiboMaxTerms, parameters.fiboMaxValue, parameters.fiboMaxFactor, parameters.fiboNbrOfLoops, parameters.numberRun)
+    fibonacci_interop_cython_full(parameters.fiboStart, parameters.fiboMaxTerms, parameters.fiboMaxValue,
+                                  parameters.fiboMaxFactor, parameters.fiboNbrOfLoops, parameters.numberRun)
+
 
 def main_python():
     nameTest: str = "Python"
     execute_loop(nameTest, execute_python)
 
+
 def main_cython():
     nameTest: str = "Cython"
     execute_loop(nameTest, execute_cython)
 
+
 def main_cpp_dll():
     nameTest: str = "C++ DLL"
     execute_loop(nameTest, execute_cpp)
+
 
 def main_cpp_asm():
     nameTest: str = "ASM x64 DLL"
@@ -157,9 +178,9 @@ def main_cpp_asm():
 
 
 if __name__ == "__main__":
-    # main_python()
-    # main_cython()
-    # main_cython_full()
-     main_dotnet()
-    # main_cpp_dll()
-    # main_cpp_asm()
+    main_python()
+    main_cython()
+    main_cython_full()
+    main_dotnet()
+    main_cpp_dll()
+    main_cpp_asm()
